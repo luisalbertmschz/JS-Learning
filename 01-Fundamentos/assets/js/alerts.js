e560e345a692aaf22b8c4b7aa657d08f0fab19b9
Luis A Morla Sánchez <luislabertmsanchez@gmail.com>
2024-02-15 01:33:22 -0400

    Actualización de avances

--- 01-Fundamentos/assets/js/alerts.js
+++ 01-Fundamentos/assets/js/alerts.js
@@ -8,7 +8,7 @@ let nombre = prompt('¿Cuál es tu nombre?');
 console.log( nombre );
 console.log( '****' + nombre + '****' ); // ''
 
-const seleccion = confirm('¿Está seguro de borrar esto?');  
+const seleccion = confirm('¿Está seguro de borrar esto?');
 console.log( seleccion );
 
 // console.log( global );
